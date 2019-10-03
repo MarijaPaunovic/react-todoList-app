9d20d0d77ef2daf76c94f3b8e21eea98cec5cb4f
MarijaPaunovic <marijapaunovicdesign@gmail.com>
2019-10-03 15:54:12 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import AddTodo from './components/todos/AddTodo';
+import About from './components/pages/About';
 import Todos from './components/todos/Todos';
 
 import './App.css';
@@ -59,17 +61,24 @@ addTodo = (title) => {
 
   render() {
   return (
-    <div className="App">
-      <div className='container'> 
-      <Header />
-      <AddTodo addTodo={this.addTodo}/>
-      <Todos 
-        todos={this.state.todos} 
-        markComplete={this.markComplete} 
-        delTodo={this.delTodo}
-      />
+    <Router>
+      <div className="App">
+        <div className='container'> 
+        <Header />
+        <Route exact path='/' render={props => (
+          <React.Fragment>
+            <AddTodo addTodo={this.addTodo}/>
+            <Todos 
+              todos={this.state.todos} 
+              markComplete={this.markComplete} 
+              delTodo={this.delTodo}
+            />
+          </React.Fragment>
+        )} />
+        <Route path='/about' component={About} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
   }
 }
